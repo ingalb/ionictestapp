8748d47e400e1601f604ea5cdb5820a1b98fb941
ingalb <contact@ingalb.info>
2014-11-02 21:31:03 +0100

    test

--- www/js/app.js
+++ www/js/app.js
@@ -8,13 +8,11 @@ angular.module('vllaznia', ['ionic', 'vllaznia.services', 'vllaznia.controllers'
 //angular.module('starter', ['angular-carousel'])
 
 .run(function($ionicPlatform) {
-  alert("OK 1");
    var gaPlugin;
   $ionicPlatform.ready(function() {
-   alert("OK");
    gaPlugin = window.plugins.gaPlugin;
    gaPlugin.init(successHandler, errorHandler, "UA-2341193-8", 10);
-   
+   alert("OK ready");
    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
